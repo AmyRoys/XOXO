2958f2db43f9a37f46fc4136cfadd76ee2493bd8
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-01-25 10:46:24 +0000

    Add 3 more content containers and arrows

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,12 +1,35 @@
+import React, { useState } from 'react';
 import './Content.css';
 import serena from './assets/serena.jpeg';
+import blair from './assets/blair.jpg';
+import Jenny_Humphrey_Season_3 from './assets/Jenny_Humphrey_Season_3.jpg';
+import dan from './assets/dan.jpg';
 
 const Content = () => {
+    const contents = [
+        { title: 'Spotted: S is back in town!!', image: serena, text: 'After taking a much needed break from the UES, Serena van der Woodsen has returned. We\'ve missed you, S!' },
+        { title: "Blair Waldorf's Secret Affair Revealed!",image: blair, text: "Upper East Side's Queen B, Blair Waldorf, caught in a scandalous rendezvous that even Gossip Girl couldn't predict!" },
+        { title: "Jenny's Grand Debut: A Night of Secrets Unveiled!",image: Jenny_Humphrey_Season_3, text: "Little J's rise to the top is just beginning. XOXO, Gossip Girl." },
+        { title: "Dan Humphrey: The Ultimate Insider?",image: dan, text: "Dan Humphrey, the outsider, is now the ultimate insider. How will this affect his relationship with Serena?" },
+    ];
+
+    const [index, setIndex] = useState(0);
+
+    const nextContent = () => {
+        setIndex((index + 1) % contents.length);
+    };
+
+    const prevContent = () => {
+        setIndex((index - 1 + contents.length) % contents.length);
+    };
+
     return (
         <div className="content-container">
-            <h2><p>Spotted: S is back in town!!</p></h2>
-            <img src={serena} alt="Serena" style= {{width: 300}} />
-            <p>After taking a much needed break from the UES, Serena van der Woodsen has returned. We've missed you, S!</p>
+            <h2>{contents[index].title}</h2>
+            <img src={contents[index].image} alt="Content" style={{ width: 250, height: 250 }} />
+            <p>{contents[index].text}</p>
+            <button onClick={prevContent}>&larr;</button>
+            <button onClick={nextContent}>&rarr;</button>
         </div>
     );
 };
